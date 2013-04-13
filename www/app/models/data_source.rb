27ca095c53f92d1fa0b96ac152a31af919cb8f14
Jon Gillies <jon@gillies.us>
2013-04-13 11:57:52 -0700

    Removed rspec controller tests, they should be done with cucumber.  Added rspec test for the data_sources model.

--- www/app/models/data_source.rb
+++ www/app/models/data_source.rb
@@ -1,3 +1,12 @@
 class DataSource < ActiveRecord::Base
+
+  rolify
+
+  validates :name, :presence => true, :uniqueness => true
+  validates :contact, :presence => true
+  validates :description, :presence => true
+
+  devise :database_authenticatable
+
   attr_accessible :active, :connection_info, :contact, :description, :name
 end
--- www/spec/models/data_source_spec.rb
+++ www/spec/models/data_source_spec.rb
@@ -1,5 +1,79 @@
 require 'spec_helper'
 
 describe DataSource do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "minimum attributes required" do
+    before(:each) do
+      @attr = {
+          :name        => "Data Set 1",
+          :contact     => "user@example.com",
+          :description => "description",
+      }
+    end
+
+    it "should create a new instance given valid attributes" do
+      DataSource.create!(@attr)
+    end
+
+    it "should require a name" do
+      no_name_data_source = DataSource.new(@attr.merge(:name => ""))
+      no_name_data_source.should_not be_valid
+    end
+
+    it "should require a contact" do
+      no_contact_data_source = DataSource.new(@attr.merge(:contact => ""))
+      no_contact_data_source.should_not be_valid
+    end
+
+    it "should require a description" do
+      no_description_data_source = DataSource.new(@attr.merge(:description => ""))
+      no_description_data_source.should_not be_valid
+    end
+
+    it "should reject duplicate data_source.name" do
+      DataSource.create!(@attr)
+      data_source_with_duplicate_name = DataSource.new(@attr)
+      data_source_with_duplicate_name.should_not be_valid
+    end
+
+  end
+
+  describe "maximum attributes" do
+    before(:each) do
+      @attr = {
+          :name            => "Data Set 1",
+          :contact         => "user@example.com",
+          :description     => "description",
+          :active          => true,
+          :connection_info => "connection info"
+      }
+    end
+
+    it "should create a new instance given valid attributes" do
+      DataSource.create!(@attr)
+    end
+
+    it "should require a name" do
+      no_name_data_source = DataSource.new(@attr.merge(:name => ""))
+      no_name_data_source.should_not be_valid
+    end
+
+    it "should require a contact" do
+      no_contact_data_source = DataSource.new(@attr.merge(:contact => ""))
+      no_contact_data_source.should_not be_valid
+    end
+
+    it "should require a description" do
+      no_description_data_source = DataSource.new(@attr.merge(:description => ""))
+      no_description_data_source.should_not be_valid
+    end
+
+    it "should reject duplicate data_source.name" do
+      DataSource.create!(@attr)
+      data_source_with_duplicate_name = DataSource.new(@attr)
+      data_source_with_duplicate_name.should_not be_valid
+    end
+
+  end
+
 end
