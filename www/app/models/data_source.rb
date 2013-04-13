5dad694528dd3d0e966f3bd3b9daae2370230ee1
Jon Gillies <jon@gillies.us>
2013-04-13 12:24:20 -0700

    Added seed data for data sources

--- www/app/models/data_source.rb
+++ www/app/models/data_source.rb
@@ -1,12 +1,8 @@
 class DataSource < ActiveRecord::Base
 
-  rolify
-
   validates :name, :presence => true, :uniqueness => true
   validates :contact, :presence => true
   validates :description, :presence => true
-
-  devise :database_authenticatable
 
   attr_accessible :active, :connection_info, :contact, :description, :name
 end
--- www/db/seeds.rb
+++ www/db/seeds.rb
@@ -25,3 +25,8 @@ YAML::load_file('db/seeds/roles_and_users.yml')['users'].each do |_, user|
   end
   puts "User> #{db_user.name} #{db_user.email}"
 end
+
+YAML::load_file('db/seeds/data_sources.yml').each do |_, data_source|
+  ds = DataSource.find_or_create_by_name(data_source, :without_protection => true)
+  puts "DataSource> #{ds.name}"
+end
