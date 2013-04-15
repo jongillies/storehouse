7dbf1815db0dc0903bd23d31982eaf725598ef9b
Jon Gillies <jon@gillies.us>
2013-04-14 21:59:26 -0700

    Added cron_schedule field to data_sources

--- www/db/schema.rb
+++ www/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130412233043) do
+ActiveRecord::Schema.define(:version => 20130415045537) do
 
   create_table "data_sources", :force => true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20130412233043) do
     t.text     "connection_info"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "cron_schedule"
   end
 
   create_table "roles", :force => true do |t|
