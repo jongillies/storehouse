e35a5b7b0b3c31d10cb065dc3ed6ebdcfa42f03f
Jon Gillies <jon@gillies.us>
2013-04-15 15:09:31 -0700

    Removed left over steps from device tests

--- www/features/step_definitions/user_steps.rb
+++ www/features/step_definitions/user_steps.rb
@@ -9,13 +9,6 @@ def find_user
   @user ||= User.where(email: @visitor[:email]).first
 end
 
-def create_unconfirmed_user
-  create_visitor
-  delete_user
-  sign_up
-  visit '/users/sign_out'
-end
-
 def create_user
   create_visitor
   delete_user
@@ -25,17 +18,6 @@ end
 def delete_user
   @user ||= User.where(email: @visitor[:email]).first
   @user.destroy unless @user.nil?
-end
-
-def sign_up
-  delete_user
-  visit '/users/sign_up'
-  fill_in 'user_name', with: @visitor[:name]
-  fill_in 'user_email', with: @visitor[:email]
-  fill_in 'user_password', with: @visitor[:password]
-  fill_in 'user_password_confirmation', with: @visitor[:password_confirmation]
-  click_button 'Sign up'
-  find_user
 end
 
 def sign_in
@@ -64,9 +46,6 @@ Given /^I do not exist as a user$/ do
   delete_user
 end
 
-Given /^I exist as an unconfirmed user$/ do
-  create_unconfirmed_user
-end
 
 ### WHEN ###
 When /^I sign in with valid credentials$/ do
@@ -76,48 +55,6 @@ end
 
 When /^I sign out$/ do
   visit '/users/sign_out'
-end
-
-When /^I sign up with valid user data$/ do
-  create_visitor
-  #sign_up
-end
-
-When /^I sign up with an invalid email$/ do
-  create_visitor
-  @visitor = @visitor.merge(email: 'notanemail')
-  sign_up
-end
-
-When /^I sign up without a password confirmation$/ do
-  create_visitor
-  @visitor = @visitor.merge(password_confirmation: '')
-  sign_up
-end
-
-When /^I sign up without a password$/ do
-  create_visitor
-  @visitor = @visitor.merge(password: '')
-  sign_up
-end
-
-When /^I sign up with a to short password$/ do
-  create_visitor
-  @visitor = @visitor.merge(password: 'short', password_confirmation: 'short')
-  sign_up
-end
-
-When $/ do
-  create_visitor
-  @visitor = @visitor.merge(password: '')
-  sign_up
-end
-
-
-When /^I sign up with a mismatched password confirmation$/ do
-  create_visitor
-  @visitor = @visitor.merge(password_confirmation: 'changeme123')
-  sign_up
 end
 
 When /^I return to the site$/ do
@@ -134,41 +71,20 @@ When /^I sign in with a wrong password$/ do
   sign_in
 end
 
-When /^I edit my account details$/ do
-  click_link 'Edit account'
-  fill_in 'user_name', with: 'newname'
-  fill_in 'user_current_password', with: @visitor[:password]
-  click_button 'Update'
-end
-
-When /^I look at the list of users$/ do
-  visit '/'
-end
-
 ### THEN ###
 Then /^I should be signed in$/ do
   page.should have_content 'Logout'
-  #page.should_not have_content 'Sign up'
   page.should_not have_content 'Login'
 end
 
 Then /^I should be signed out$/ do
-  #page.should have_content 'Sign up'
   page.should have_content 'Login'
   page.should_not have_content 'Logout'
-end
-
-Then /^I see an unconfirmed account message$/ do
-  page.should have_content 'You have to confirm your account before continuing.'
 end
 
 Then /^I see a successful sign in message$/ do
   page.should have_content 'Signed in successfully.'
 end
-
-#Then /^I should see a successful sign up message$/ do
-#  page.should have_content 'Welcome! You have signed up successfully.'
-#end
 
 Then /^I should see an invalid email message$/ do
   page.should have_content 'is invalid'
@@ -178,31 +94,10 @@ Then /^I should see a missing password message$/ do
   page.should have_content 'can\'t be blank'
 end
 
-Then /^I should see a missing password confirmation message$/ do
-  page.should have_content 'doesn\'t match confirmation'
-end
-
-Then /^I should see a mismatched password message$/ do
-  page.should have_content 'doesn\'t match confirmation'
-end
-
-Then /^I should see a password is too short message$/ do
-  page.should have_content 'is too short'
-end
-
 Then /^I should see a signed out message$/ do
   page.should have_content 'Signed out successfully.'
 end
 
 Then /^I see an invalid login message$/ do
   page.should have_content 'Invalid email or password.'
-end
-
-Then /^I should see an account edited message$/ do
-  page.should have_content 'You updated your account successfully.'
-end
-
-Then /^I should see my name$/ do
-  create_user
-  page.should have_content @user[:name]
 end
--- www/features/users/sign_in.feature
+++ www/features/users/sign_in.feature
@@ -3,12 +3,6 @@ Feature: Sign in
   A user
   Should be able to sign in
 
-    Scenario: User is not signed up
-      Given I do not exist as a user
-      When I sign in with valid credentials
-      Then I see an invalid login message
-        And I should be signed out
-
     Scenario: User signs in successfully
       Given I exist as a user
       When I sign in with valid credentials
