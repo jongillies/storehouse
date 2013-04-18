83ce6c931420ca65b1b2f2d2c0164fbf47b9f4bc
Jon Gillies <jon@gillies.us>
2013-04-18 09:41:04 -0700

    refactored the edit/destroy in the show to a helper. Added the record count in the index.

--- www/app/helpers/data_sources_helper.rb
+++ www/app/helpers/data_sources_helper.rb
@@ -1,2 +1,16 @@
 module DataSourcesHelper
+
+  def link_to_edit(resource, klass = "btn")
+    link_to t('.edit', :default => t("helpers.links.edit")),
+            edit_data_source_path(resource), :class => klass
+  end
+
+  def link_to_destroy(resource, klass = "btn btn-danger")
+    link_to t('.destroy', :default => t("helpers.links.destroy")),
+            data_source_path(resource),
+            :method => :delete,
+            :data   => {:confirm => t('.confirm', :default => t("helpers.links.confirm", :default => 'Are you sure?'))},
+            :class  => klass
+  end
+
 end
