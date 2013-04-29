b913f1519ad6580a5754b7bf81715b731554c0aa
Jon Gillies <jon@gillies.us>
2013-04-29 10:10:09 -0700

    Fixed the spec tests.  Have run into a problem with the cucumber test for data sources.  In the index we link to the list of export runs for the data source.  Normally this would be an active record relation or nil, but when using FactoryGirl data_source.export_runs is an empty array.  So in the index when we test for "if data_source.export_runs" it will work in the "real" app but not in cucumber.  Not sure what to do about that at this point.

--- www/spec/routing/export_records_routing_spec.rb
+++ www/spec/routing/export_records_routing_spec.rb
@@ -4,31 +4,15 @@ describe ExportRecordsController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/export_records").should route_to("export_sets#index")
-    end
-
-    it "routes to #new" do
-      get("/export_records/new").should route_to("export_records#new")
+      get("/export_records").should route_to("export_records#index")
     end
 
     it "routes to #show" do
-      get("/export_records/1").should route_to("export_sets#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      get("/export_records/1/edit").should route_to("export_records#edit", :id => "1")
+      get("/export_records/1").should route_to("export_records#show", :id => "1")
     end
 
     it "routes to #create" do
-      post("/export_records").should route_to("export_sets#create")
-    end
-
-    it "routes to #update" do
-      put("/export_records/1").should route_to("export_records#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      delete("/export_records/1").should route_to("export_records#destroy", :id => "1")
+      post("/export_records").should route_to("export_records#create")
     end
 
   end
--- www/spec/routing/export_runs_routing_spec.rb
+++ www/spec/routing/export_runs_routing_spec.rb
@@ -7,28 +7,12 @@ describe ExportRunsController do
       get("/export_runs").should route_to("export_runs#index")
     end
 
-    it "routes to #new" do
-      get("/export_runs/new").should route_to("export_runs#new")
-    end
-
     it "routes to #show" do
       get("/export_runs/1").should route_to("export_runs#show", :id => "1")
     end
 
-    it "routes to #edit" do
-      get("/export_runs/1/edit").should route_to("export_runs#edit", :id => "1")
-    end
-
     it "routes to #create" do
       post("/export_runs").should route_to("export_runs#create")
-    end
-
-    it "routes to #update" do
-      put("/export_runs/1").should route_to("export_runs#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      delete("/export_runs/1").should route_to("export_runs#destroy", :id => "1")
     end
 
   end
