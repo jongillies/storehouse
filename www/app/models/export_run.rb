1bc3f1d7527bea675ef3bd8e9cd8d890efdf30e1
Jon Gillies <jon@gillies.us>
2013-05-01 10:44:25 -0700

    Fixed to duration would be calculated at save only if not present and started_at and finished_at are present.  Need to write some tests for this.

--- www/app/models/export_run.rb
+++ www/app/models/export_run.rb
@@ -5,14 +5,16 @@ class ExportRun < ActiveRecord::Base
   belongs_to :data_source
   has_many :export_records
 
-  before_create :update_duration
+  before_save :update_duration
 
   validates :data_source_id, :presence => true
 
   private
 
   def update_duration
-    self.duration = finished_at - started_at
+    if !duration && finished_at && started_at
+      self.duration = finished_at - started_at
+    end
   end
 
 end
