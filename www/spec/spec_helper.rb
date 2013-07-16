bb84ab3a2ec8f60759ddcee91a75399f96275642
Jon Gillies <jon@gillies.us>
2013-07-16 16:15:17 -0700

    Added simplercov

--- www/spec/spec_helper.rb
+++ www/spec/spec_helper.rb
@@ -7,6 +7,9 @@ require 'email_spec'
 require 'rspec/autorun'
 require 'database_cleaner'
 
+require 'simplecov'
+SimpleCov.start
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
