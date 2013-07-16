2358c1c3b69576bf64b5454dbb3e3e27d9e563ed
Jon Gillies <jon@gillies.us>
2013-07-15 17:06:21 -0700

    Holy crap, it works!  Need to write tests now.

--- www/app/controllers/api/data_sources_controller.rb
+++ www/app/controllers/api/data_sources_controller.rb
@@ -1,37 +1,37 @@
 class Api::DataSourcesController < RocketPants::Base
-  # GET /data_sources
-  # GET /data_sources.json
 
-  #map_error! CanCan::AccessDenied, RocketPants::Forbidden
+  include Devise::Controllers::Helpers
+  include CanCan::ControllerAdditions
 
-  #include CanCan::ControllerAdditions
-  #include CanCan::Ability
+  before_filter :authenticate_user!
+
+  #TODO: https://github.com/filtersquad/rocket_pants/issues/7
+  #TODO: https://github.com/filtersquad/rocket_pants/issues/43
+  #TODO: https://gist.github.com/lukebyrne/3224885
+
+  map_error! CanCan::AccessDenied, RocketPants::Forbidden
 
   def index
-    #authorize! :read, DataSource #, message: 'Not authorized for API use.' if current_user
+    authorize! :read, DataSource, message: 'Not authorized for API use.'
 
     @search = DataSource.search(params[:q])
     expose @search.result.order(:name).page params[:page]
   end
 
-  # GET /data_sources/1
-  # GET /data_sources/1.json
   def show
+    authorize! :read, DataSource, message: 'Not authorized for API use.'
+
     expose DataSource.find(params[:id])
   end
 
-  # POST /data_sources
-  # POST /data_sources.json
   def create
-    #authorize! :create, DataSource, message: 'Not authorized as an administrator.'
+    authorize! :create, DataSource, message: 'Not authorized as an administrator.'
 
     expose DataSource.create!(params[:data_source]), status: :created
   end
 
-  # PUT /data_sources/1
-  # PUT /data_sources/1.json
   def update
-    #authorize! :update, DataSource, message: 'Not authorized as an administrator.'
+    authorize! :update, DataSource, message: 'Not authorized as an administrator.'
 
     @data_source = DataSource.find(params[:id])
 
@@ -40,20 +40,13 @@ class Api::DataSourcesController < RocketPants::Base
     expose @data_source
   end
 
-  # DELETE /data_sources/1
-  # DELETE /data_sources/1.json
   def destroy
-    #authorize! :destroy, DataSource, message: 'Not authorized as an administrator.'
+    authorize! :destroy, DataSource, message: 'Not authorized as an administrator.'
 
     @data_source = DataSource.find(params[:id])
     @data_source.destroy
 
     redirect_to data_source_path version: 1
   end
-
-  def default_serializer_options
-    {root: false}
-  end
-
 
 end
