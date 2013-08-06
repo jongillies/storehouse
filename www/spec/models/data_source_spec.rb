436b920bbcf20e36a3f545dff0bf49e5d63d206d
Jon Gillies <jon@gillies.us>
2013-08-06 13:01:52 -0700

    Refactored specs to remove unused attributes.  Updated readme.

--- www/spec/models/data_source_spec.rb
+++ www/spec/models/data_source_spec.rb
@@ -2,35 +2,35 @@ require 'spec_helper'
 
 describe DataSource do
 
-  describe "minimum attributes required" do
+  describe 'minimum attributes required' do
     before(:each) do
       @attr = {
-          :name        => "Data Set 1",
-          :contact     => "user@example.com",
-          :description => "description",
+          name: 'Data Set 1',
+          contact: 'user@example.com',
+          description: 'description',
       }
     end
 
-    it "should create a new instance given valid attributes" do
+    it 'should create a new instance given valid attributes' do
       DataSource.create!(@attr)
     end
 
-    it "should require a name" do
-      no_name_data_source = DataSource.new(@attr.merge(:name => ""))
+    it 'should require a name' do
+      no_name_data_source = DataSource.new(@attr.merge(:name => ''))
       no_name_data_source.should_not be_valid
     end
 
-    it "should require a contact" do
-      no_contact_data_source = DataSource.new(@attr.merge(:contact => ""))
+    it 'should require a contact' do
+      no_contact_data_source = DataSource.new(@attr.merge(:contact => ''))
       no_contact_data_source.should_not be_valid
     end
 
-    it "should require a description" do
-      no_description_data_source = DataSource.new(@attr.merge(:description => ""))
+    it 'should require a description' do
+      no_description_data_source = DataSource.new(@attr.merge(:description => ''))
       no_description_data_source.should_not be_valid
     end
 
-    it "should reject duplicate data_source.name" do
+    it 'should reject duplicate data_source.name' do
       DataSource.create!(@attr)
       data_source_with_duplicate_name = DataSource.new(@attr)
       data_source_with_duplicate_name.should_not be_valid
@@ -38,38 +38,36 @@ describe DataSource do
 
   end
 
-  describe "maximum attributes" do
+  describe 'maximum attributes' do
     before(:each) do
       @attr = {
-          :name            => "Data Set 1",
-          :contact         => "user@example.com",
-          :description     => "description",
-          :active          => true,
-          :primary_store => "connection info",
-          :secondary_stores => "connection info"
+          name: 'Data Set 1',
+          contact: 'user@example.com',
+          description: 'description',
+          active: true
       }
     end
 
-    it "should create a new instance given valid attributes" do
+    it 'should create a new instance given valid attributes' do
       DataSource.create!(@attr)
     end
 
-    it "should require a name" do
-      no_name_data_source = DataSource.new(@attr.merge(:name => ""))
+    it 'should require a name' do
+      no_name_data_source = DataSource.new(@attr.merge(:name => ''))
       no_name_data_source.should_not be_valid
     end
 
-    it "should require a contact" do
-      no_contact_data_source = DataSource.new(@attr.merge(:contact => ""))
+    it 'should require a contact' do
+      no_contact_data_source = DataSource.new(@attr.merge(:contact => ''))
       no_contact_data_source.should_not be_valid
     end
 
-    it "should require a description" do
-      no_description_data_source = DataSource.new(@attr.merge(:description => ""))
+    it 'should require a description' do
+      no_description_data_source = DataSource.new(@attr.merge(:description => ''))
       no_description_data_source.should_not be_valid
     end
 
-    it "should reject duplicate data_source.name" do
+    it 'should reject duplicate data_source.name' do
       DataSource.create!(@attr)
       data_source_with_duplicate_name = DataSource.new(@attr)
       data_source_with_duplicate_name.should_not be_valid
