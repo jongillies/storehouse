b53667fecf36497f4be655915d94733048340c31
Jon Gillies <jon@gillies.us>
2013-08-08 18:00:58 -0700

    Removed bin, moved to tests..

--- bin/c1.sh
+++
@@ -1,8 +0,0 @@
-
-json='{"data_source_id":1,"started_at":"2013-05-02 15:02:55 -0700"}'
-
-
-curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"data_source_id":1,"started_at":"2013-05-02 15:07:29 -0700"}' "http://localhost:3000/export_runs?auth_token=z0000000000000000000"
-
-
-
